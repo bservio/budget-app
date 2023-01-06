84c115b75c2b40565b6aa62d7fb5f0ee9ce97b9a
Bruno Sérvio Barbosa <brunoservio21@gmail.com>
2023-01-06 00:11:24 -0300

    First checkpoint

---
+++ src/contexts/BudgetContext.jsx
@@ -0,0 +1,66 @@
+import React, { useContext, useState } from "react";
+import { v4 as uuidV4 } from "uuid"
+import useLocalStorage from "../hooks/useLocalStorage"
+
+const BudgetsContext = React.createContext()
+
+export const UNCATEGORIZED_BUDGET_ID = "Uncategorized"
+
+export function useBudgets() {
+	return useContext(BudgetsContext);
+}
+
+
+export const BudgetsProvider = ({ children }) => {
+	const [budgets, setBudgets] = useLocalStorage("budgets", [])
+	const [expenses, setExpenses] = useLocalStorage("expenses", [])
+
+	budgetId: "13ecbe6c-9772-4dc6-99fa-f413e629af90"
+
+	function getBudgetExpenses(budgetId) {
+		return expenses.filter(expense => expense.budgetId === budgetId)
+
+	}
+
+	function addExpense({ description, amount, budgetId }) {
+		setExpenses(prevExpenses => {
+			return [...prevExpenses, { id: uuidV4(), description, amount, budgetId }]
+		})
+	}
+
+	function addBudget({ budgetName, max }) {
+		setBudgets(prevBudgets => {
+			if (prevBudgets.find(budget => budget.budgetName === budgetName)) {
+				return prevBudgets
+			}
+			return [...prevBudgets, { id: uuidV4(), budgetName, max }]
+		})
+	}
+	function deleteExpense({ id }) {
+		setExpenses(prevExpenses => {
+			return prevExpenses.filter(expense => expense.id !== id)
+		})
+	}
+
+	function deleteBudget({ id }) {
+		//TODO: deal with deletion of budgets
+		setBudgets(prevBudgets => {
+			return prevBudgets.filter(budget => budget.id !== id)
+		})
+	}
+	return (
+		<BudgetsContext.Provider value={
+			{
+				budgets,
+				expenses,
+				getBudgetExpenses,
+				addExpense,
+				addBudget,
+				deleteExpense,
+				deleteBudget
+			}
+		}>
+			{children}
+		</BudgetsContext.Provider>
+	)
+}
